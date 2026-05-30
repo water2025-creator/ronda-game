086b7a632e047f1932ad757c878702ef121c9cd4
M2termine <votre-email@exemple.com>
2026-05-31 01:19:14 +0300

    🎴 Initial commit Ronda - Build OK

---
+++ next.config.js
@@ -0,0 +1,9 @@
+﻿/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true },
+  trailingSlash: true,
+  swcMinify: true,
+  compress: true,
+};
+module.exports = nextConfig;
